78360586a6ef40ddec8686828794381a48e3d0a1
Larify <luoweihua7@gmail.com>
2024-01-30 17:06:15 +0800

    feat: api response json

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,15 @@ const getKV = (env: Env) => {
   return new KVConfig(env.UrlKV)
 }
 
+const createJSONResponse = (data: any) => {
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+  })
+}
+
 export default {
   async add(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const kv = getKV(env);
@@ -11,7 +20,7 @@ export default {
     console.log('/api/add', key, value)
     const ret = await kv.set(key, value)
 
-    return new Response(`Add ${key}=${ret}`)
+    return createJSONResponse({ code: 0, data: ret })
   },
   async get(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const kv = getKV(env);
@@ -19,7 +28,7 @@ export default {
     console.log('/api/get', key)
     const ret = await kv.get(key)
 
-    return new Response(`Get ${key}=${ret}`)
+    return createJSONResponse({ code: 0, data: ret })
   },
   async delete(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const kv = getKV(env);
@@ -27,7 +36,7 @@ export default {
     console.log('/api/delete', key)
     const ret = await kv.delete(key)
 
-    return new Response(`Delete ${key}=${ret}`)
+    return createJSONResponse({ code: 0, data: ret })
   },
   async list(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const kv = getKV(env);
@@ -35,6 +44,6 @@ export default {
     console.log('/api/list', prefix, limit)
     const keys = await kv.list(prefix, limit);
 
-    return new Response(`List ${JSON.stringify(keys)}`)
+    return createJSONResponse({ code: 0, data: keys })
   }
 }
