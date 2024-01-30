0b38151cdc22a2175894cfe174bdcdf8a9f337d6
Larify <luoweihua7@gmail.com>
2024-01-30 17:17:52 +0800

    feat: add prefix to key

--- src/services/api.ts
+++ src/services/api.ts
@@ -13,36 +13,38 @@ const createJSONResponse = (data: any) => {
   })
 }
 
+const KEY_PREFIX = 'SHORT_URL_KEY:'
+
 export default {
   async add(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const kv = getKV(env);
     const { key, value } = await request.json();
     console.log('/api/add', key, value)
-    const ret = await kv.set(key, value)
+    const ret = await kv.set(`${KEY_PREFIX}${key}`, value)
 
-    return createJSONResponse({ code: 0, data: ret })
+    return createJSONResponse({ code: 0, data: { key, ret } })
   },
   async get(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const kv = getKV(env);
     const { key } = await request.params ?? {};
     console.log('/api/get', key)
-    const ret = await kv.get(key)
+    const ret = await kv.get(`${KEY_PREFIX}${key}`)
 
-    return createJSONResponse({ code: 0, data: ret })
+    return createJSONResponse({ code: 0, data: { key, ret } })
   },
   async delete(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const kv = getKV(env);
     const { key } = request.params ?? {}
     console.log('/api/delete', key)
-    const ret = await kv.delete(key)
+    const ret = await kv.delete(`${KEY_PREFIX}${key}`)
 
-    return createJSONResponse({ code: 0, data: ret })
+    return createJSONResponse({ code: 0, data: { key, ret } })
   },
   async list(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const kv = getKV(env);
     const { prefix, limit } = await request.query;
-    console.log('/api/list', prefix, limit)
-    const keys = await kv.list(prefix, limit);
+    console.log('/api/list', prefix || KEY_PREFIX, limit)
+    const keys = await kv.list(prefix || KEY_PREFIX, limit);
 
     return createJSONResponse({ code: 0, data: keys })
   }
